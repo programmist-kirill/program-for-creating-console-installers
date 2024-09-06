7002d31fadc66b93b3ce8b8494b3ffbfdec64b5f
kirill <150036551+programmist-kirill@users.noreply.github.com>
2024-09-06 22:22:50 +0600

    Add files via upload

--- information_fake.py
+++ information_fake.py
@@ -1,74 +1,51 @@
 import os
 from termcolor import colored
-
 import print_user
-
 class Information_package:
     def main():
         directory_is_information_file = input('Введите директорию файла "information" :')
         if os.path.exists(directory_is_information_file):
-            
             with open(directory_is_information_file, 'r') as file:
                 lines = file.readlines()
-        
-        else:
-            print(colored('Отсутствует директория файла "information"','red'))
-            input('\n\nДля продолжения нажмите ENTER')
-            sys.exit()
-
-        with open('setup_cashe/package/name_package.txt', 'w') as name_package:
-            name_package.write(lines[0])
-
-        with open('setup_cashe/package/version_package.txt', 'w') as version_package:
-            version_package.write(lines[1])
-        
-        with open('setup_cashe/package/url_package.txt', 'w') as url_package:
-            url_package.write(lines[2])
-
-        print_user.dowloaded_package.main()
-
+            with open('setup_cashe/package/name_package.txt', 'w') as name_package:
+                name_package.write(lines[0])
+            with open("setup_cashe/package/version_package.", 'w') as version_package:
+                 version_package.write(lines[1])
+            with open("setup_cashe/package/url_package.txt", "w") as url_package:
+                 url_package.write(lines[2])
+            print_user.dowloaded_package.main()
 class File_package:
     def main():
-        
         directory_setup_list_file = input('Введите директорию файла "setup.list" :')
         if os.path.exists(directory_setup_list_file):
             with open(directory_setup_list_file, 'r') as file:
-                lines = file.read().split('\n')
-        
+                lines = file.read().split("n")
         else:
             print(colored('Отсутствует директория файла "setup.list"','red'))
             input('\n\nДля продолжения нажмите ENTER')
             sys.exit()
-
         try:
-            print(colored('\n\n\n\nstart install package','green'))
+            print(colored('\n\n\nstart install package','green'))
             #index = количество строк в файле setup.list
             index_input = len(lines)
             index_input = int(index_input)
             index_output = 0
-            while index_input != index_output:
-                if index_input != index_output:
+            while index_input!= index_output:
+                if index_input!= index_output:
                     name = lines[index_output]
-                    with open('setup_cashe/file' ,'w') as fp:
+                    with open('setup_cashe/file', 'w') as fp:
                         fp.write(name)
                     print_user.copy_package.main(name)
-                    
                     index_output += 1
-
         except Exception as e:
             print(colored('Error install package','red'))
             print(e)
             input('\n\nДля продолжения нажмите ENTER')
             sys.exit()
-            
-        print(colored('end install package','green'))
-
-
+    print(colored('end install package','green'))
 class Install_package:
     def main():
         print_user.install_package.main()
-
-
 Information_package.main()
 File_package.main()
 Install_package.main()
--- print_user_fake.py
+++ print_user_fake.py
@@ -2,25 +2,18 @@ import os
 import sys
 import shutil
 from termcolor import colored
-
-
 class dowloaded_package:
     def main():
         print(colored('start function dowloaded package','green'))
-        
         with open('setup_cashe/package/url_package.txt', 'r') as file:
             url_package = file.read().strip()
-        
         try:
             os.system('git clone ' + url_package)
         except:
-            print(colored('Error git clone ' + url_package,'red'))
+            print(colored('Error git clone '+ url_package,'red'))
             input('\n\nДля продолжения нажмите ENTER')
             sys.exit()
-
         print(colored('end function dowloaded package','green'))
-
-
 class copy_package:
     def main(name):
         with open('setup_cashe/package/name_package.txt', 'r') as name_package:
@@ -39,32 +32,26 @@ class copy_package:
 
 class install_package:
     def main():
-
         with open('setup_cashe/package/name_package.txt', 'r') as name_package:
             name_package = name_package.read().strip('\n')  # удалить символ новой строки
-
-        print(colored('start function install package','green'))
+        print(colored('start install package','green'))
         if os.path.exists(name_package + '/install.py'):
             try:
                 os.system('python ' + name_package + '/install.py')
-                os.remove(name_package)
             except Exception as e:
-                print(f"\n" + {e})
-                print(colored('\nError install package','red'))
+                print(f'\n' + {e})
+                print(colored('Error install package','red'))
                 input('\n\nДля продолжения нажмите ENTER')
                 sys.exit()
-            print(colored('end funct on install package','green'))
-
+            print(colored('end install package','green'))
         else:
             if os.path.exists('install'):
                 try:
-                    os.system('sudo chmod +x "./install"')
-                    os.system('"./install"')
-                    os.remove(name_package)
+                    os.system('sudo chmod +x \"./install\"')
+                    os.system('./install')
                 except Exception as e:
-                    print(f"\n" + {e})
-                    print(colored('\nError install package','red'))
-            else:
-                print(colored('Not found script install package','yellow'))
-
-            print(colored('end funct on install package','green'))
+                    print(f'\n' + {e})
+                    print(colored('Error install package','red'))
+                    input('\n\nДля продолжения нажмите ENTER')
+                    sys.exit()
+                print(colored('end install package','green'))
